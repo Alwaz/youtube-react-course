ab69018814bc5f4e7e512a8445c835739073150e
Alwaz <alwazkazi3@gmail.com>
2025-11-27 22:22:22 +0500

    hw(module-8): abstract search component logic in custom hook

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,67 +1,41 @@
+import { useSearch } from "@/lib/useSearch";
 import { Search } from "lucide-react";
-import { useState } from "react";
-import useSearchStore from "../store/searchStore";
-import useBaseMovieStore from "@/store/moviesStore";
-import performSearch from "../lib/performSearch";
 
 export default function SearchBar() {
-    const [shouldShowSearch, setShouldShowSearch] = useState(false);
+  const { shouldShowSearch, setShouldShowSearch, handleSearchQueryChange } =
+    useSearch();
 
-    const baseMovies = useBaseMovieStore(state => state.baseMovies);
-    const setResults = useSearchStore(state => state.setResults);
+  const handleBlur = () => {
+    setShouldShowSearch(false);
+  };
+  const handleSearchClick = () => {
+    setShouldShowSearch(true);
+  };
 
-
-    const handleBlur = () => {
-        setShouldShowSearch(false);
-    }
-    const handleSearchClick = () => {
-        setShouldShowSearch(true);
-    };
-
-
-    const searchQuery = (query: string) => {
-        const matchingTitles = performSearch(query, baseMovies);
-        console.log('Matching Titles:', matchingTitles.data);
-        setResults(matchingTitles.data);
-    };
-
-
-    const handleSearchQueryChange = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        const query = event.target.value;
-
-        searchQuery(query);
-
-    };
-
-    return (
-        <div className="flex items-center">
-            {shouldShowSearch ? (
-                <div className="flex items-center bg-black/80 border border-white/20 rounded-sm px-3 py-2 min-w-[280px] backdrop-blur-sm">
-                    <Search
-                        size={20}
-                        className="text-white/70 mr-3 flex-shrink-0"
-                    />
-                    <input
-                        className="bg-transparent text-white placeholder:text-white/60 text-sm focus:outline-none flex-1 font-normal"
-                        type="text"
-                        placeholder="Titles, people, genres"
-                        aria-label="Search"
-                        onChange={handleSearchQueryChange}
-                        onBlur={handleBlur}
-                        autoFocus
-                    />
-                </div>
-            ) : (
-                <button
-                    onClick={handleSearchClick}
-                    className="p-2 hover:bg-white/10 rounded-sm transition-colors duration-200"
-                    aria-label="Search"
-                >
-                    <Search size={24} className="text-white" />
-                </button>
-            )}
+  return (
+    <div className="flex items-center">
+      {shouldShowSearch ? (
+        <div className="flex items-center bg-black/80 border border-white/20 rounded-sm px-3 py-2 min-w-[280px] backdrop-blur-sm">
+          <Search size={20} className="text-white/70 mr-3 shrink-0" />
+          <input
+            className="bg-transparent text-white placeholder:text-white/60 text-sm focus:outline-none flex-1 font-normal"
+            type="text"
+            placeholder="Titles, people, genres"
+            aria-label="Search"
+            onChange={handleSearchQueryChange}
+            onBlur={handleBlur}
+            autoFocus
+          />
         </div>
-    );
+      ) : (
+        <button
+          onClick={handleSearchClick}
+          className="p-2 hover:bg-white/10 rounded-sm transition-colors duration-200"
+          aria-label="Search"
+        >
+          <Search size={24} className="text-white" />
+        </button>
+      )}
+    </div>
+  );
 }
---
+++ src/lib/useSearch.ts
@@ -0,0 +1,31 @@
+import useBaseMovieStore from "@/store/moviesStore";
+import useSearchStore from "@/store/searchStore";
+import { useState } from "react";
+import performSearch from "./performSearch";
+
+export function useSearch() {
+  const [shouldShowSearch, setShouldShowSearch] = useState(false);
+
+  const baseMovies = useBaseMovieStore((state) => state.baseMovies);
+  const setResults = useSearchStore((state) => state.setResults);
+
+  const searchQuery = (query: string) => {
+    const matchingTitles = performSearch(query, baseMovies);
+    console.log("Matching Titles:", matchingTitles.data);
+    setResults(matchingTitles.data);
+  };
+
+  const handleSearchQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const query = event.target.value;
+
+    searchQuery(query);
+  };
+  return {
+    shouldShowSearch,
+    setShouldShowSearch,
+
+    handleSearchQueryChange,
+  };
+}
